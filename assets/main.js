202eb66392d74f47ded582f6d9a77b32b7e1fb5e
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-05 14:01:34 +0200

    add main js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,15 @@
+//Chiedi all’utente il suo nome
 
+const Name = prompt("qual è il tuo nome?");
+
+//chiedi il suo cognome
+
+const Surname = prompt("qual è il tuo cognome?");
+
+//chiedi il suo colore preferito
+
+const Color = prompt("qual è il tuo colore preferito?");
+
+//scrivi sulla pagina nomecognomecolorepreferito22
+
+document.getElementById('psw').innerHTML = Name + Surname + Color + '22';
