ff3d92d2410bf02022dec4f1bccb715128014adc
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-05 16:08:31 +0200

    add details and correction uppercase

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,19 @@
 //Chiedi all’utente il suo nome
 
-const Name = prompt("qual è il tuo nome?");
+const firstname = prompt("qual è il tuo nome?");
 
 //chiedi il suo cognome
 
-const Surname = prompt("qual è il tuo cognome?");
+const surname = prompt("qual è il tuo cognome?");
 
 //chiedi il suo colore preferito
 
-const Color = prompt("qual è il tuo colore preferito?");
+const color = prompt("qual è il tuo colore preferito?");
+
+const currentYear = "22"
+
+const textPsw = `${firstname + surname + color + currentYear}`;
 
 //scrivi sulla pagina nomecognomecolorepreferito22
 
-document.getElementById('psw').innerHTML = Name + Surname + Color + '22';
+document.getElementById('psw').innerHTML = textPsw;
